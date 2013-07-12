33f347d3c692b2b196ffc431ab241b387e12dde4
Cody Gray <cody.cg@gmail.com>
2013-07-12 13:58:21 -0600

    better formatted the data.

--- debugger.js
+++ debugger.js
@@ -1,7 +1,7 @@
-var tracking_data = window.opener.tracking_data
+var tracking_data = window.opener.tracking_data;
 
-document.write("tracking_data:");
+document.write("tracking_data:<br>");
 
 for(var name in tracking_data){
-  document.write(name + ':' + tracking_data[name] + '\n');
+  document.write(name + ':' + tracking_data[name] + '<br>');
 }
