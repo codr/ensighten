b63997f63fad22925f3aea163323cfe750995711
Cody Gray <cody.cg@gmail.com>
2013-07-12 13:22:12 -0600

    fixed variable naming error.

--- debugger.js
+++ debugger.js
@@ -3,5 +3,5 @@ var tracking_data = window.opener.tracking_data
 document.write("tracking_data:");
 
 for(var name in tracking_data){
-  document.write(n + ':' + tracking_data[n] + '\n');
+  document.write(name + ':' + tracking_data[name] + '\n');
 }
